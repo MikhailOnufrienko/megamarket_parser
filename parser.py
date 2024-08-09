b08c886b2ee24a699501460fb9572a25f3959dc7
Mikhail Onufrienko <m.onufrienko@eteron.ru>
2024-08-09 12:08:07 +0400

    Add exceptions.

--- parser.py
+++ parser.py
@@ -1,11 +1,10 @@
 import time
 
 import pandas as pd
-from pydantic import Field, ValidationError, HttpUrl
+from pydantic import ValidationError
 
 from selenium.common.exceptions import TimeoutException, WebDriverException, NoSuchElementException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.support import expected_conditions as EC
@@ -28,12 +27,19 @@ class Parser:
                 for cookie in cookies:
                     driver.add_cookie(cookie)
             time.sleep(2)
-            product_elements = WebDriverWait(driver, 10).until(
-                EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'a[data-test="product-name-link"]'))
-            )
+            try:
+                product_elements = WebDriverWait(driver, 10).until(
+                    EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'a[data-test="product-name-link"]'))
+                )
+            except TimeoutException:
+                print('Не удалось загрузить элементы продуктов на странице.')
+                return
             if product_elements:
-                products: Product = Parser._extract(product_elements, driver)
-                Parser._save_to_excel(products)
+                try:
+                    products: Product = Parser._extract(product_elements[:1], driver)
+                    Parser._save_to_excel(products)
+                except Exception as e:
+                    print(f'Ошибка при извлечении данных или сохранении в Excel: {e}')
     
     @staticmethod
     def _is_captcha_present(driver: WebDriver) -> bool:
@@ -47,42 +53,57 @@ class Parser:
     @staticmethod
     def _price_to_float(price: str) -> float:
         # Преобразовать строкое представление цены в тип float.
-        price_cleaned = price.replace(" ", "").replace("₽", "")
-        return round(float(price_cleaned), 2)
+        try:
+            price_cleaned = price.replace(" ", "").replace("₽", "")
+            return round(float(price_cleaned), 2)
+        except ValueError:
+            print(f'Не удалось преобразовать цену "{price}" в тип float.')
+            return 0.00
     
     @staticmethod
     def _extract(elements: list[WebElement], driver: WebDriver) -> list[Product]:
+        # Извлечь данные из найденных веб-элементов.
         products = []
         for element in elements:
             try:
                 link = element.get_attribute('href')
-                print(link)
+                driver.get(link)
+                time.sleep(2)
+                name=driver.find_element(By.CSS_SELECTOR, 'h1[itemprop="name"]').text
+                price=driver.find_element(By.CSS_SELECTOR, 'span.sales-block-offer-price__price-final').text
+                try:
+                    description=driver.find_element(By.CSS_SELECTOR, 'div[itemprop="description"].text-block').text
+                except NoSuchElementException:
+                    continue
+                try:
+                    product = Product(
+                        name=name,
+                        link=link,
+                        price=Parser._price_to_float(price),
+                        description=description,
+                    )
+                    products.append(product)
+                except ValidationError as e:
+                    print(f'Не удалось создать объект Product: {e}')
+                    continue
+            except WebDriverException as e:
+                print(f'Произошла ошибка при парсинге страницы: {e}')
+                return []
             except Exception as e:
-                print(f'ERROR: {e}')
-            driver.get(link)
-            time.sleep(2)
-            name=driver.find_element(By.CSS_SELECTOR, 'h1[itemprop="name"]').text
-            price=driver.find_element(By.CSS_SELECTOR, 'span.sales-block-offer-price__price-final').text
-            try:
-                description=driver.find_element(By.CSS_SELECTOR, 'div[itemprop="description"].text-block').text
-            except NoSuchElementException:
-                continue
-            product = Product(
-                name=name,
-                link=link,
-                price=Parser._price_to_float(price),
-                description=description,
-            )
-            products.append(product)
+                print(f'Произошла непредвиденная ошибка при парсинге страницы: {e}')
+                return []
         return products
 
     @staticmethod
     def _save_to_excel(products: list[Product], filename='goods.xlsx') -> None:
         # Сохранить извлеченные данные в таблицу Excel.
-        df = pd.DataFrame([product.__dict__ for product in products])
-        df['description'] = df['description'].fillna("")
-        with pd.ExcelWriter(filename, engine='openpyxl') as writer:
-            df.to_excel(writer, sheet_name='goods', index=False)
+        try:
+            df = pd.DataFrame([product.__dict__ for product in products])
+            df['description'] = df['description'].fillna("")
+            with pd.ExcelWriter(filename, engine='openpyxl') as writer:
+                df.to_excel(writer, sheet_name='goods', index=False)
+        except Exception as e:
+            print(f'Ошибка при сохранении данных в Excel: {e}')
 
 
 if __name__ == '__main__':
